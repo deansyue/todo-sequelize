899c9eff1e492eb7b8892861581709fc2d89ac54
deansyue <c1318131801@gmail.com>
2022-01-19 22:02:37 +0800

    project init

---
+++ app.js
@@ -0,0 +1,26 @@
+//引入模組
+const express = require('express')
+const exphbs = require('express-handlebars').engine
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 3000
+
+//設定網頁handlebars相關設定
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+// 使用 express內建的body - parser 進行前置處理
+app.use(express.urlencoded({ extended: true }))
+// 設置每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
+
+//首頁路由
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+//監聽伺服器
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
