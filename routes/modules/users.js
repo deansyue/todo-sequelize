484b7deebf0666a82d342df9b33252bfc7093510
deansyue <c1318131801@gmail.com>
2022-01-20 23:08:26 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,12 @@
 //引入模組
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
+
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
 
 //設定login表單的路由
 router.get('/login', (req, res) => {
@@ -20,7 +25,25 @@ router.get('/register', (req, res) => {
 
 //設定註冊表回傳資料的路由
 router.post('/register', (req, res) => {
-  res.render('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('user is already exists')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 //設定登出路由
