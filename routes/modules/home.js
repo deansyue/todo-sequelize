39a04378a79a731d9b5072f92c0cee59a6a663c4
deansyue <c1318131801@gmail.com>
2022-01-20 22:50:19 +0800

    feat: add home router and index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,9 +3,18 @@ const express = require('express')
 
 const router = express.Router()
 
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
+
 //首頁路由
 router.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true,
+  })
+    .then((todos) => { return res.render('index', { todos }) })
+    .catch(err => { return res.status(422).json(err) })
 })
 
 //匯出模組
