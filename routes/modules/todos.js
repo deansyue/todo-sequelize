30517ed3b8033db9b0226b3d948bf0b2db7b50b3
deansyue <c1318131801@gmail.com>
2022-01-21 21:58:41 +0800

    feat: add edit of user routers and edit page

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -40,12 +40,33 @@ router.get('/:id', (req, res) => {
 
 //設定按下Edit後的路由
 router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(err => console.log(err))
 
 })
 
 //設定使用save傳送出修改表單的路由
 router.put('/:id', (req, res) => {
+  //取得userId id及表單資料
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
 
+  //查詢修改的指定資料
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      //將從資料庫資料name與isDone修改，並回傳至資料庫
+      todo.name = name
+      todo.isDone = (isDone === 'on')
+      return todo.save()
+    })
+    //重新導向detail頁面
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(err => console.log(err))
 })
 
 //設定使用delete後的路由
